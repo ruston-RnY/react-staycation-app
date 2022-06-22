2acd101d31d8b2aa601169a4b34d870f3afdd8dd
ruston.efendi <ruston.efendi@redtech.co.id>
2022-06-22 14:22:27 +0700

    finish mostpicked, categories component

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -3,6 +3,7 @@ import Header from "../parts/Header";
 import landingPage from "../assets/json/landingPage.json";
 import Hero from "../parts/Hero";
 import MostPicked from "../parts/MostPicked";
+import Categories from "../parts/Categories";
 
 export default class LandingPage extends Component {
   constructor(props) {
@@ -11,15 +12,15 @@ export default class LandingPage extends Component {
   }
 
   render() {
-    console.log(this.props);
     return (
       <>
-        <Header {...this.props}></Header>
-        <Hero refMostPicked={this.refMostPicked} data={landingPage.hero}></Hero>
+        <Header {...this.props} />
+        <Hero refMostPicked={this.refMostPicked} data={landingPage.hero} />
         <MostPicked
           refMostPicked={this.refMostPicked}
           data={landingPage.mostPicked}
-        ></MostPicked>
+        />
+        <Categories data={landingPage.categories} />
       </>
     );
   }
