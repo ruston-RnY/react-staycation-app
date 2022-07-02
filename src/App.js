d8a4e2612ddb4585fdc821cb2771ac5fd8e6ec00
ruston.efendi@redtech.co.id <ruston.efendi@redtech.co.id>
2022-07-02 14:50:21 +0700

    add component input text, input file

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import "./assets/scss/style.scss";
 import LandingPage from "./pages/LandingPage";
 import DetailPage from "./pages/DetailPage";
+import Checkout from "./pages/Checkout";
 import Example from "./pages/Example";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/properties/:id" element={<DetailPage />} />
+        <Route path="/checkout" element={<Checkout />} />
         <Route path="/tes" element={<Example />} />
       </Routes>
     </div>
